fc185a628c80270682dd364c722568df5ff9e17b
SnowDev <peregrine.asbell@rsu35.org>
2024-11-13 12:55:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:10000');
 });
