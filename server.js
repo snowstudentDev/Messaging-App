0366e858fb43cb9eff39285a161ba29035ea6a7c
SnowDev <peregrine.asbell@rsu35.org>
2024-11-14 09:14:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,37 +6,23 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+let users = [];
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('New user connected');
 
-    socket.on('setName', (name) => {
-        socket.username = name;
-        io.emit('userJoined', name);
-    });
-
-    socket.on('createGroup', (groupName) => {
-        const groupId = Math.random().toString(36).substring(2, 15);
-        socket.join(groupId); // Automatically join the creator to the group
-        io.emit('groupCreated', { groupName, groupId });
-    });
-
-    socket.on('joinGroup', (groupId) => {
-        socket.join(groupId);
-        io.to(groupId).emit('userJoinedGroup', socket.username);
-    });
-
-    socket.on('sendMessage', (data) => {
-        const { groupId, message } = data;
-        io.to(groupId).emit('receiveMessage', { user: socket.username, message });
+    socket.on('join', (username) => {
+        users.push({ id: socket.id, name: username });
+        io.emit('userList', users); // Send updated user list to all clients
     });
 
     socket.on('disconnect', () => {
+        users = users.filter(user => user.id !== socket.id);
+        io.emit('userList', users); // Update user list on disconnect
         console.log('User  disconnected');
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:10000');
+server.listen(process.env.PORT || 10000, () => {
+    console.log('Server is running');
 });
