2e2f6d3434f20f120fd97b4d237ce17444363cf7
SnowDev <peregrine.asbell@rsu35.org>
2024-11-14 09:24:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,46 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let users = [];
+let servers = {}; // Store server details
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+    console.log('A user connected');
 
-    socket.on('join', (username) => {
-        users.push({ id: socket.id, name: username });
-        io.emit('userList', users); // Send updated user list to all clients
+    socket.on('createServer', ({ serverName, password }) => {
+        if (!servers[serverName]) {
+            servers[serverName] = { password, users: [] };
+            socket.join(serverName);
+            socket.emit('serverCreated', { serverName });
+        } else {
+            socket.emit('error', 'Server already exists');
+        }
+    });
+
+    socket.on('joinServer', ({ serverName, password }) => {
+        const server = servers[serverName];
+        if (server && server.password === password) {
+            socket.join(serverName);
+            server.users.push(socket.id);
+            io.to(serverName).emit('userList', server.users);
+            socket.emit('joinedServer', { serverName });
+        } else {
+            socket.emit('error', 'Invalid server name or password');
+        }
     });
 
     socket.on('disconnect', () => {
-        users = users.filter(user => user.id !== socket.id);
-        io.emit('userList', users); // Update user list on disconnect
-        console.log('User  disconnected');
+        console.log('A user disconnected');
+        for (const serverName in servers) {
+            const server = servers[serverName];
+            const index = server.users.indexOf(socket.id);
+            if (index !== -1) {
+                server.users.splice(index, 1);
+                io.to(serverName).emit('userList', server.users);
+            }
+        }
     });
 });
 
-server.listen(process.env.PORT || 10000, () => {
-    console.log('Server is running');
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
